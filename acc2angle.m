6d1db211692fef3baf43f384ea1124bffb1516e9
Muun <52774019+Muun-Muun@users.noreply.github.com>
2022-06-29 17:03:21 +0900

    Change the initial conditions

--- acc2angle.m
+++ acc2angle.m
@@ -2,14 +2,12 @@ function angle_cmd = acc2angle(acc)
 % acc = [ax ay ax]
 global roll0
 
-yaw = atan2(acc(2),acc(1));
-%pitch = -atan2(norm([acc(2) acc(1)]),-acc(3));
-%pitch = pi + atan2(acc(1),acc(3));
-pitch = -atan2(norm([acc(2) acc(1)]),-acc(3));
+yaw = atan(acc(2)/acc(1));
+pitch = -atan(norm([acc(2) acc(1)])/-acc(3));
 %pitch = atan2(-acc(1),-acc(3));
-% if pitch > 0
-%     pitch = pitch - 2*pi;
-% end
+if pitch > 0
+    pitch = pitch + 2*pi;
+end
 
 roll = 0;
 
--- attitude_controller.m
+++ attitude_controller.m
@@ -13,8 +13,8 @@ phi_cmd = angle_cmd(1);
 theta_cmd = angle_cmd(2);
 psi_cmd = angle_cmd(3);
 
-Moment_cmd(1,1) = -K_P*P + K_phi*acos(cos((phi_cmd - phi)));
+Moment_cmd(1,1) = -K_P*P + K_phi*(phi_cmd - phi);
 Moment_cmd(1,2) = -K_Q*Q + K_theta*(theta_cmd - theta);
-Moment_cmd(1,3) = -K_R*R + K_psi*acos(cos((psi_cmd - psi)));
+Moment_cmd(1,3) = -K_R*R + K_psi*(psi_cmd - psi);
 
 end
